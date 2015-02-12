2d2d1d9aeba9a1ef48548e7e8a00b906d6ff83ed
Francisco Gamonal <francisco741@hotmail.com>
2015-02-12 11:59:52 -0500

    title page

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -1,5 +1,5 @@
-@extends('layouts.mep')
+@extends('layouts/mep')
 
 @section('menu')
-	Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla ipsam omnis, nemo quam atque harum natus perferendis neque necessitatibus tempore at quo similique, iste debitis quasi consequuntur temporibus explicabo obcaecati.
+	<p>hola</p>
 @endsection
--- resources/views/layouts/mep.blade.php
+++ resources/views/layouts/mep.blade.php
@@ -19,23 +19,29 @@
 </head>
 <body>
 	<div class="nav-wrapper">
+		@yield('menu')
 		<div class="logo paddingWrapper">
 			<figure><a href="{{ url('/') }}"><img class="center-block" src="{{ asset('img/mep-logo.png') }}"></a></figure>
 		</div>
-		@yield('menu')
 		<ul class="nav">
 			<li><a href="#">Home</a></li>
 			<li><a href="#">Profile</a></li>
 			<li><a href="#">Messages</a></li>
 		</ul>
-		<div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde ipsum dolorum eius voluptates, velit vero facilis. Esse aliquid tempora a nam voluptates nobis aspernatur, velit eligendi, aut, laborum at quod?</div>
-		
 	</div>
 	<div class="content-wrapper ">
+		@yield('message')
 		<div class="message border-bottom">
 			Message
 		</div>
-		<div id="content" class="paddingWrapper"> @yield('content')
+		@yield('content')
+		<aside class="page"> 
+			<h2>Home	</h2>
+			<ul>
+				<li class="active">Home</li>
+			</ul>
+		</aside>
+		<div id="content" class="paddingWrapper">
 			<div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque a repellat dolorem fuga, fugiat facere, voluptatem odio hic consequatur commodi qui repellendus, iusto, quae reprehenderit id eius! Magnam, ex, laborum.</div>
 			<div>Eveniet culpa quo eligendi. Non consequuntur in quod officia, dicta. Dicta veniam labore aliquam, odit et rem quos maiores, dolor maxime expedita, ratione corrupti. Eius sapiente neque, exercitationem tempora iure!</div>
 			<div>Reprehenderit, blanditiis laborum cupiditate illo eius fuga voluptates cumque labore numquam ducimus quaerat aliquid commodi debitis pariatur iste minima dicta, beatae natus repudiandae sequi earum dolore at dolorem. Cumque, necessitatibus!</div>
