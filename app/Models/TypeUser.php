9feb427c564ce33de6f462c23ed8886469f62c9a
Anwar Sarmiento <anwarsarmiento@gmail.com>
2015-03-04 23:22:25 -0600

    listo type

--- app/Models/TypeUser.php
+++ app/Models/TypeUser.php
@@ -1,6 +1,7 @@
 <?php namespace Mep\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Response;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Mep\User;
 
@@ -27,5 +28,25 @@ class TypeUser extends Model {
     public function users(){
         
         return $this->belongsToMany('Users');
+    }
+    public function LastId() {
+        return TypeUser::all()->last();
+    }
+
+    public function isValid($data) {
+        $rules = ['name' => 'required|unique:menus'];
+
+        if ($this->exists) {
+            $rules['name'] .= ',name,' . $this->id;
+        }
+
+        $validator = \Validator::make($data, $rules);
+        if ($validator->passes()) {
+            return true;
+        }
+
+        $this->errors = $validator->errors();
+
+        return false;
     }
 }
