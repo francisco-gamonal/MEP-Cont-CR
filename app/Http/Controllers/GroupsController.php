86efc065188abcef4ed68a184b4400adefb31391
Anwar Sarmiento <anwarsarmiento@gmail.com>
2015-04-04 20:36:39 -0600

    update groups

--- app/Http/Controllers/GroupsController.php
+++ app/Http/Controllers/GroupsController.php
@@ -1,44 +1,44 @@
-<?php namespace Mep\Http\Controllers;
+<?php
+
+namespace Mep\Http\Controllers;
 
 use Mep\Http\Requests;
 use Mep\Http\Controllers\Controller;
 use Mep\Models\Group;
 use Illuminate\Http\Request;
 use Input;
+
 class GroupsController extends Controller {
 
-	/**
-	 * Display a listing of the resource.
-	 *
-	 * @return Response
-	 */
-	public function index()
-	{
-            $groups = Group::all();
-            return view('groups.index',  compact('groups'));
-	}
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Response
+     */
+    public function index() {
+        $groups = Group::all();
+        return view('groups.index', compact('groups'));
+    }
 
-	/**
-	 * Show the form for creating a new resource.
-	 *
-	 * @return Response
-	 */
-	public function create()
-	{
-             return view('groups.create');
-	}
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return Response
+     */
+    public function create() {
+        return view('groups.create');
+    }
 
-	/**
-	 * Store a newly created resource in storage.
-	 *
-	 * @return Response
-	 */
-	public function store()
-	{
-		 /* Capturamos los datos enviados por ajax */
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @return Response
+     */
+    public function store() {
+        /* Capturamos los datos enviados por ajax */
         $Groups = $this->convertionObjeto();
         /* Creamos un array para cambiar nombres de parametros */
-        $ValidationData = array('name' => $Groups->nameGroup,'code' => $Groups->codeGroup);
+        $ValidationData = array('name' => $Groups->nameGroup, 'code' => $Groups->codeGroup);
         /* Declaramos las clases a utilizar */
         $group = new Group;
         /* Validamos los datos para guardar tabla menu */
@@ -60,81 +60,75 @@ class GroupsController extends Controller {
         endif;
         /* Enviamos el mensaje de error */
         return $this->errores($group->errors);
-	}
+    }
 
-	/**
-	 * Display the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function show($id)
-	{
-		//
-	}
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function show($id) {
+        //
+    }
 
-	/**
-	 * Show the form for editing the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function edit($token)
-	{
-            $group = Group::withTrashed()->where('token', '=', $token)->get();
-            return view('groups.edit', compact('group'));
-	}
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function edit($token) {
+        $group = Group::withTrashed()->where('token', '=', $token)->get();
+        return view('groups.edit', compact('group'));
+    }
 
-	/**
-	 * Update the specified resource in storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function update($token)
-	{
-            echo json_encode($token); die;
-		  /* Capturamos los datos enviados por ajax */
-        $groups = $this->convertionObjeto();
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function update() {
+        /* Capturamos los datos enviados por ajax */
+        $Groups = $this->convertionObjeto();
         /* Creamos un array para cambiar nombres de parametros */
-        $ValidationData = array('name' => $Groups->nameGroup,'code' => $Groups->codeGroup);
+        $ValidationData = array('name' => $Groups->nameGroup, 'code' => $Groups->codeGroup);
         /* Declaramos las clases a utilizar */
-        $group = Group::withTrashed()->where('token', '=', $token);
+        $group = Group::Token( $Groups->token);
         /* Validamos los datos para guardar tabla menu */
-        if ($group->isValid((array) $ValidationData)):
+        if ($group->isValid((array)$ValidationData)):
             $group->code = strtoupper($ValidationData['code']);
             $group->name = strtoupper($ValidationData['name']);
             $group->save();
             /* Comprobamos si viene activado o no para guardarlo de esa manera */
-            if ($groups->statusGroup == true):
-                Group::withTrashed()->where('token', '=', $token)->restore();
+            if ($Groups->statusGroup == true):
+                Group::withTrashed()->where('token', '=', $Groups->token)->restore();
             else:
-                Group::destroy()->where('token', '=', $token);
+                Group::destroy()->where('token', '=', $Groups->token);
             endif;
             /* Enviamos el mensaje de guardado correctamente */
             return $this->exito('Los datos se guardaron con exito!!!');
         endif;
         /* Enviamos el mensaje de error */
         return $this->errores($group->errors);
-	}
+    }
 
-	/**
-	 * Remove the specified resource from storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	 public function destroy() {
-        /* Capturamos los datos enviados por ajax */
-        $group = $this->convertionObjeto();
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function destroy($id) {
         /* les damos eliminacion pasavida */
-        $data = Group::destroy($group->idGroup);
+        $data = Group::destroy($id);
         if ($data):
             /* si todo sale bien enviamos el mensaje de exito */
             return $this->exito('Se desactivo con exito!!!');
         endif;
         /* si hay algun error  los enviamos de regreso */
-       return $this->errores($data->errors);
+        return $this->errores($data->errors);
     }
 
     /**
@@ -143,18 +137,16 @@ class GroupsController extends Controller {
      * @param  int  $id
      * @return Response
      */
-    public function active() {
-        /* Capturamos los datos enviados por ajax */
-        $group = $this->convertionObjeto();
+    public function active($id) {
         /* les quitamos la eliminacion pasavida */
-        $data = Group::onlyTrashed()->find($group->idGroup);
+        $data = Group::onlyTrashed()->find($id);
         if ($data):
             $data->restore();
             /* si todo sale bien enviamos el mensaje de exito */
             return $this->exito('Se Activo con exito!!!');
         endif;
         /* si hay algun error  los enviamos de regreso */
-       return $this->errores($data->errors);
+        return $this->errores($data->errors);
     }
 
 }
