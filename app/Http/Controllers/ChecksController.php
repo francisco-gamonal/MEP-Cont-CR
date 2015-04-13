f0ce5b207e9e2de8917344ae462685765d73f960
Anwar Sarmiento <anwarsarmiento@gmail.com>
2015-04-12 22:16:08 -0600

    retorno de arreglo balance budget

--- app/Http/Controllers/ChecksController.php
+++ app/Http/Controllers/ChecksController.php
@@ -13,21 +13,21 @@ use Mep\Models\Spreadsheet;
 
 class ChecksController extends Controller {
 
+    public function budget($token) {
+        $spreadsheets = Spreadsheet::Token($token);
+        $balancebudgets = BalanceBudget::where('budgets_id', '=', $spreadsheets[0]->budgets_id)->get();
+        return $balancebudgets;
+    }
 
-	public function budget($token){
-		return $token;
-	}
-
-	/**
-	 * Display a listing of the resource.
-	 *
-	 * @return Response
-	 */
-	public function index()
-	{
-            $checks = Check::withTrashed()->get();
-            return view('checks.index', compact('checks'));
-	}
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Response
+     */
+    public function index() {
+        $checks = Check::withTrashed()->get();
+        return view('checks.index', compact('checks'));
+    }
 
     /**
      * Show the form for creating a new resource.
@@ -37,8 +37,8 @@ class ChecksController extends Controller {
     public function create() {
         $voucher = Voucher::all();
         $suppliers = Supplier::all();
-        $spreadsheets = Spreadsheet::orderBy('number','ASC')->orderBy('year','ASC')->get();
-        $balancebudgets = BalanceBudget::where('budgets_id','=',$spreadsheets[0]->budgets_id)->get();
+        $spreadsheets = Spreadsheet::orderBy('number', 'ASC')->orderBy('year', 'ASC')->get();
+        $balancebudgets = BalanceBudget::where('budgets_id', '=', $spreadsheets[0]->budgets_id)->get();
         return view('checks.create', compact('voucher', 'suppliers', 'spreadsheets', 'balancebudgets'));
     }
 
@@ -51,17 +51,17 @@ class ChecksController extends Controller {
         /* Capturamos los datos enviados por ajax */
         $checks = $this->convertionObjeto();
         /* Consulta por token de school */
-        $voucher= Voucher::Token($checks->voucherCheck);
-        $supplier= Supplier::Token($checks->voucherCheck);
-        $spreadsheet= Spreadsheet::Token($checks->voucherCheck);
-        $balanceBudget= BalanceBudget::Token($checks->voucherCheck);
+        $voucher = Voucher::Token($checks->voucherCheck);
+        $supplier = Supplier::Token($checks->voucherCheck);
+        $spreadsheet = Spreadsheet::Token($checks->voucherCheck);
+        $balanceBudget = BalanceBudget::Token($checks->voucherCheck);
         /* Creamos un array para cambiar nombres de parametros */
         $ValidationData = $this->CreacionArray($checks, 'Check');
         /* Asignacion de id de school */
-        $ValidationData['vouchers_id']=$voucher->id;
-        $ValidationData['suppliers_id']=$supplier->id;
-        $ValidationData['spreadsheets_id']=$spreadsheet->id;
-        $ValidationData['balance_budgets_id']=$balanceBudget->id;
+        $ValidationData['vouchers_id'] = $voucher->id;
+        $ValidationData['suppliers_id'] = $supplier->id;
+        $ValidationData['spreadsheets_id'] = $spreadsheet->id;
+        $ValidationData['balance_budgets_id'] = $balanceBudget->id;
         /* Declaramos las clases a utilizar */
         $check = new Check;
         /* Validamos los datos para guardar tabla menu */
@@ -70,8 +70,8 @@ class ChecksController extends Controller {
             $check->save();
             /* Traemos el id del tipo de usuario que se acaba de */
             $idCheck = $check->LastId();
-            /*Actualizacion de la table balance*/
-            BalanceController::saveBalance($balanceBudgets->amountBalanceBudget,'entrada','false','balance_budgets_id',$idBalanceBudget->id,$balanceBudgets->statusBalanceBudget);
+            /* Actualizacion de la table balance */
+            BalanceController::saveBalance($balanceBudgets->amountBalanceBudget, 'entrada', 'false', 'balance_budgets_id', $idBalanceBudget->id, $balanceBudgets->statusBalanceBudget);
             /* Comprobamos si viene activado o no para guardarlo de esa manera */
             if ($checks->statusCheck == true):
                 Check::withTrashed()->find($idCheck->id)->restore();
