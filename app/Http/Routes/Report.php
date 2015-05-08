e468946832c200772f638866e9895fdab1ee84dc
Anwar Sarmiento <anwarsarmiento@gmail.com>
2015-05-07 23:23:05 -0600

    ruta correccion

--- app/Http/Routes/Report.php
+++ app/Http/Routes/Report.php
@@ -5,4 +5,4 @@
 Route::get('reporte/presupuesto', ['as' => 'reporte-presupuesto', 'uses' => 'ReportController@index']);
 Route::get('reporte/presupuesto/excel/{token}', ['as' => 'reporte-presupuesto-excel', 'uses' => 'ExcelController@budgetInicialExcel']);
 Route::get('reporte/planilla/excel', ['as' => 'reporte-planilla-excel', 'uses' => 'ExcelController@excelSpreadsheet']);
-Route::get('reporte/ordinario/excel', ['as' => 'reporte-presupuesto-ordinario-excel', 'uses' => 'ExcelController@generalBudgetExcel']);
+Route::get('reporte/ordinario/excel/{token}/{global}/{year}', ['as' => 'reporte-presupuesto-ordinario-excel', 'uses' => 'ExcelController@generalBudgetExcel']);
