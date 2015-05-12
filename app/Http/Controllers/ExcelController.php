093d0defb1d32e0a5245342236584c7fdb1727c1
Anwar Sarmiento <anwarsarmiento@gmail.com>
2015-05-11 23:44:15 -0600

    Final Transfer

--- app/Http/Controllers/ExcelController.php
+++ app/Http/Controllers/ExcelController.php
@@ -21,15 +21,64 @@ class ExcelController extends Controller {
     /**
      * **************************************inicio Excel de cuadro Transferencia *************************************
      */
-    public function excelTransfers() {
-        $transfers = Transfer::where('code', 9)->get();
+    public function excelTransfers($token) {
+        $transfers = Transfer::where('token', $token)->get();
         $content = $this->CreateArrayTransfer($transfers);
+        $file= Count($content);
         $firms = $this->firmTransfers();
         foreach ($firms AS $firm):
             $content[] = $firm;
         endforeach;
-        Excel::create('Transfers-', function($excel) use ($content) {
-            $excel->sheet('Cuadro Transfers-', function($sheet) use ($content ) {
+        Excel::create('Transfers-', function($excel) use ($content,$file) {
+            $excel->sheet('Cuadro Transfers-', function($sheet) use ($content,$file ) {
+                 $sheet->mergeCells('A1:M1');
+                $sheet->mergeCells('A2:M2');
+                $sheet->mergeCells('A3:M3');
+                $sheet->mergeCells('A5:F5');
+                $sheet->mergeCells('A6:F6');
+                $sheet->mergeCells('A7:F7');
+                $sheet->setBorder('A8:F' . $file, 'thin');
+                $file=$file+1;
+                $sheet->mergeCells('A'.$file.':F'.$file);
+                $file=$file+1;
+                $sheet->mergeCells('A'.$file.':F'.$file);
+                $file=$file+1;
+                $sheet->mergeCells('A'.$file.':F'.$file);
+                $file=$file+1;
+                $sheet->mergeCells('A'.$file.':F'.$file);
+                $file=$file+1;
+                $sheet->mergeCells('A'.$file.':F'.$file);
+                $file=$file+1;
+                $sheet->mergeCells('A'.$file.':F'.$file);
+                $file=$file+1;
+                $sheet->mergeCells('A'.$file.':F'.$file);
+                $file=$file+1;
+                $sheet->mergeCells('A'.$file.':F'.$file);
+                $file=$file+1;
+                
+                $sheet->mergeCells('A'.$file.':C'.$file);
+                $sheet->mergeCells('D'.$file.':F'.$file);
+                $sheet->cells('A8:F8', function($cells) {
+                    $cells->setFontWeight(10);
+                });
+                $sheet->cells('A1:F3', function($cells) {
+                    $cells->setAlignment('center');
+                       $cells->setFontWeight('bold');
+                });
+                $sheet->cells('A4:F8', function($cells) {
+                    $cells->setAlignment('center');
+                    $cells->setValignment('center');
+                       $cells->setFontWeight('bold');
+                });
+                $file=$file-9;
+                $file1=$file+3;
+                $sheet->cells('A'.$file.':F'.$file1, function($cells) {
+                    $cells->setAlignment('left');
+                    $cells->setValignment('center');
+                       $cells->setFontWeight('bold');
+                });
+               
+                $sheet->setHeight(8, 50);
                 $sheet->fromArray($content, null, 'A1', true, false);
             });
         })->export('xls');
@@ -78,7 +127,7 @@ class ExcelController extends Controller {
             array('Revisado por Tesorero Contador:_________________________'),
             array('Aprobación (para uso exclusivo de la Dirección Regional de Educación)'),
             array('Nombre y firma del funcionario que aprueba:_______________________________________'),
-            array('Sello Dirección Regional:_______________________________________', '', '', '', '', 'Fecha de aprobación')
+            array('Sello Dirección Regional:_______________________________________', '', '', 'Fecha de aprobación________________')
         );
 
         return $firm;
