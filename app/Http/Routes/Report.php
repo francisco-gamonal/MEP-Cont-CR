dab136281d000c4370c3db8aec8f579ae434ed4b
Anwar Sarmiento <anwarsarmiento@gmail.com>
2015-05-13 00:29:58 -0600

    rutas de poa

--- app/Http/Routes/Report.php
+++ app/Http/Routes/Report.php
@@ -7,5 +7,5 @@ Route::get('reporte/presupuesto/excel/{token}', ['as' => 'reporte-presupuesto-ex
 Route::get('reporte/planilla/excel/{token}', ['as' => 'reporte-planilla-excel', 'uses' => 'ExcelController@excelSpreadsheet']);
 Route::get('reporte/transferencia/excel/{token}', ['as' => 'reporte-transfers-excel', 'uses' => 'ExcelController@excelTransfers']);
 Route::get('reporte/ordinario/excel/{token}/{global}/{year}', ['as' => 'reporte-presupuesto-ordinario-excel', 'uses' => 'ExcelController@generalBudgetExcel']);
-Route::get('reporte/saldo-de-presupuestos/excel/{token}',['as'=>'reporte-saldo-de-presupuestos','uses'=>'ExcelController@excelPoa']);
+Route::get('reporte/saldo-de-presupuestos/excel/{token}',['as'=>'reporte-saldo-de-presupuestos-poa','uses'=>'ExcelController@excelPoa']);
 Route::get('reporte/presupuestos/excel/{token}',['as'=>'reporte-presupuestos','uses'=>'ExcelController@excelPoaBudget']);
--- resources/views/balanceBudgets/index.blade.php
+++ resources/views/balanceBudgets/index.blade.php
@@ -65,7 +65,7 @@
                                 <th>Presupuesto</th>
                                 <th>Tipo de Presupuesto</th>
                                 <th>Estado</th>
-                                <th>Reporte</th>
+                                <th>Reporte POA</th>
                                 <th>Edición</th>
                             </tr>
                         </thead>
@@ -89,6 +89,7 @@
 	                                </td>
 									<td class="text-center edit-row">
 										<a href="{{route('reporte-saldo-de-presupuestos', $balanceBudget->token)}}" target="_blank"><i class="fa fa-file-pdf-o"></i></a>
+                                                                                <a href="{{route('reporte-saldo-de-presupuestos-poa', $balanceBudget->token)}}" target="_blank"><i class="fa fa-file-excel-o"></i></a>
 	                                </td>
 	                                <td class="text-center edit-row">
                                 		@if($balanceBudget->deleted_at)
