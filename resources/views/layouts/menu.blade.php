cfab261463a5b728efdccfbaae26ad979d0d0384
Francisco Gamonal <hfgamonalb@gmail.com>
2015-05-27 00:03:40 -0500

    fixed menu

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -3,63 +3,19 @@
 		<?php $menus = \Html::menu(); ?>
 		@foreach ($menus as $key => $menu)
 			@if(count($menu['tasks']) > 0)
-				<li class='submenu {{ (explode("-", Route::currentRouteName())[1] == strtolower($key) ? "active" : "" ) }}'>
+				<li class='submenu {{ (Route::currentRouteName() != "home") ? (explode("-", Route::currentRouteName())[1] == strtolower($key) ? "active" : "" ): "" }}'>
 					<a href="#">
 						<span class="glyphicon glyphicon-home"></span>
 						<span>{{mb_convert_case($key, MB_CASE_TITLE, 'utf-8')}}</span>
 						<span class="icon-menu glyphicon glyphicon-chevron-right pull-right"></span>
 					</a>
-					<ul class="nav" style='{{ (explode("-", Route::currentRouteName())[1] == strtolower($key) ? "display:block" : "display:none" ) }}'>
+					<ul class="nav" style='{{ (Route::currentRouteName() != "home") ? (explode("-", Route::currentRouteName())[1] == strtolower($key) ? "display:block" : "display:none" ) : "" }}'>
 					@foreach($menu['tasks'] as $task)
-						<li class='{{ (Route::currentRouteName() == strtolower($task->name)."-".strtolower($key) ? "active-menu" : "") }}'><a href="{{ url(strtolower($menu['url']).'/'.strtolower($task->name).'-'.strtolower($key))}}">{{$task->name}}</a></li>
+						<li class='{{ (Route::currentRouteName() != "home") ? (Route::currentRouteName() == strtolower($task->name)."-".strtolower($key) ? "active-menu" : "") : "" }}'><a href="{{ url(strtolower($menu['url']).'/'.strtolower($task->name).'-'.strtolower($key))}}">{{$task->name}}</a></li>
 					@endforeach
 					</ul>
 				</li>
 			@endif
 		@endforeach
-		<!-- <li class="active">
-			<a href="{{ url('/') }}">
-				<span class="glyphicon glyphicon-home"></span>
-				<span>Home</span>
-			</a>
-		</li>
-		<li class="submenu">
-			<a href="#">
-				<span class="glyphicon glyphicon-home"></span>
-				<span>Profile1</span>
-				<span class="icon-menu glyphicon glyphicon-chevron-right pull-right"></span>
-			</a>
-			<ul class="nav">
-				<li><a href="#">Developer</a></li>
-				<li><a href="#">DBA</a></li>
-				<li><a href="#">Servidores</a></li>
-				<li><a href="#">Servidores</a></li>
-			</ul>
-		</li>
-		<li>
-			<a href="#">
-				<span class="glyphicon glyphicon-home"></span>
-				<span>Messages</span>
-			</a>
-		</li>
-		<li class="submenu">
-			<a href="#">
-				<span class="glyphicon glyphicon-home"></span>
-				<span>Profile</span>
-				<span class="icon-menu glyphicon glyphicon-chevron-right pull-right"></span>
-			</a>
-			<ul class="nav">
-				<li><a href="#">Developer</a></li>
-				<li><a href="#">DBA</a></li>
-				<li><a href="#">Servidores</a></li>
-				<li><a href="#">Servidores</a></li>
-			</ul>
-		</li>
-		<li>
-			<a href="#">
-				<span class="glyphicon glyphicon-home"></span>
-				<span>Messages</span>
-			</a>
-		</li> -->
 	</ul>
 </div>
