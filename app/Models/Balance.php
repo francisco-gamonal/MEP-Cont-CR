6a16614632287bb177b4d8d09d1437ac008a9370
Anwar Sarmiento <anwarsarmiento@gmail.com>
2015-06-02 00:11:35 -0600

    saldo inicial de cuentas

--- app/Models/Balance.php
+++ app/Models/Balance.php
@@ -5,30 +5,26 @@ namespace Mep\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class Balance extends Model
-{
+class Balance extends Model {
+
     use SoftDeletes;
 
     // Don't forget to fill this array
-    protected $fillable = ['type', 'amount', 'simulation', 'balance_budget_id', 'check_id', 'transfer_code','transfer_balance_budget_id'];
+    protected $fillable = ['type', 'amount', 'simulation', 'balance_budget_id', 'check_id', 'transfer_code', 'transfer_balance_budget_id'];
 
-    public function checks()
-    {
+    public function checks() {
         return $this->belongsTo('Mep\Models\Check', 'check_id', 'id');
     }
 
-    public function balanceBudgets()
-    {
+    public function balanceBudgets() {
         return $this->belongsTo('Mep\Models\BalanceBudget', 'balance_budget_id', 'id');
     }
 
-    public function transfers()
-    {
+    public function transfers() {
         return $this->belongsTo('Mep\Models\Transfer');
     }
 
-    public function balanceLast($type, $amount)
-    {
+    public function balanceLast($type, $amount) {
         if ($this->type == $type) {
             return $this->amount + $amount;
         }
@@ -36,43 +32,44 @@ class Balance extends Model
         return $this->$amount - $amount;
     }
 
-    public function balanceTransfer($campo, $id)
-    {
+    public function balanceTransfer($campo, $id) {
         $balances = self::where($campo, '=', $id)->where('transfer_id', '=', $transfer)->get();
         foreach ($balances as $balance):
 
         endforeach;
     }
-    public static function BalanceInicialTotal($id, $check, $spreadsheet, $checkTransfer)
-    {
-        $balanceBudget = self::where('balance_budget_id', $id)->sum('amount',2);
+
+    public static function BalanceInicialTotal($id, $check, $spreadsheet, $checkTransfer) {
+        $balanceBudget = self::where('balance_budget_id', $id)->sum('amount', 2);
 
         /**/
 
-       $transfersEntrada = Transfer::where('transfers.spreadsheet_id', '<=', $spreadsheet->id)
-               ->where('transfers.balance_budget_id', $id)->where('transfers.type', 'entrada')->sum('transfers.amount',2);
+        $transfersEntrada = Transfer::where('transfers.spreadsheet_id', '<=', $spreadsheet->id)
+                        ->where('transfers.balance_budget_id', $id)->where('transfers.type', 'entrada')->sum('transfers.amount', 2);
 
-       /**/
-       $transfersSalida = Transfer::where('transfers.spreadsheet_id', '<=', $spreadsheet->id)
-               ->where('transfers.balance_budget_id', $id)->where('transfers.type', 'salida')->sum('transfers.amount',2);
-       /**/
         /**/
-         if (!empty($check)):
-        $checks = self::join('checks', 'checks.id', '=', 'balances.check_id')->where('check_id', '<', $check)
-                ->where('balances.balance_budget_id', $id)->where('date', $spreadsheet->date)->sum('balances.amount',2); else:
-        $checks = self::join('checks', 'checks.id', '=', 'balances.check_id')->where('spreadsheet_id', '<', $checkTransfer)
-                ->where('balances.balance_budget_id', $id)->where('date', $spreadsheet->date)->sum('balances.amount',2);
+        $transfersSalida = Transfer::where('transfers.spreadsheet_id', '<=', $spreadsheet->id)
+                        ->where('transfers.balance_budget_id', $id)->where('transfers.type', 'salida')->sum('transfers.amount', 2);
+        /**/
+        $checkIn = Check::where('spreadsheet_id', '<', $spreadsheet->id)
+                        ->where('balance_budget_id', $id)->sum('amount', 2);
+        /**/
+        if (!empty($check)):
+            $checks = self::join('checks', 'checks.id', '=', 'balances.check_id')->where('check_id', '<', $check)
+                            ->where('balances.balance_budget_id', $id)->where('date', $spreadsheet->date)->sum('balances.amount', 2);
+        else:
+            $checks = self::join('checks', 'checks.id', '=', 'balances.check_id')->where('spreadsheet_id', '<', $checkTransfer)
+                            ->where('balances.balance_budget_id', $id)->where('date', $spreadsheet->date)->sum('balances.amount', 2);
         endif;
-        $balance = ($balanceBudget + $transfersEntrada) - ($checks + $transfersSalida);
+        $balance = ($balanceBudget + $transfersEntrada) - ($checks + $transfersSalida + $checkIn);
 
         return $balance;
     }
 
-    public function isValid($data)
-    {
+    public function isValid($data) {
         $rules = ['type' => 'required',
             'amount' => 'required',
-            'simulation' => 'required', ];
+            'simulation' => 'required',];
 
         $validator = \Validator::make($data, $rules);
         if ($validator->passes()) {
@@ -83,4 +80,5 @@ class Balance extends Model
 
         return false;
     }
+
 }
