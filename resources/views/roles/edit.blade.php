bedb3d1871e2fde4a7ff3ebef8241345522c1c76
Francisco Gamonal <hfgamonalb@gmail.com>
2015-06-03 08:40:40 -0500

    edit role Check

--- resources/views/roles/edit.blade.php
+++ resources/views/roles/edit.blade.php
@@ -67,19 +67,21 @@
 						</div>
 						@foreach($menu->Tasks as $taskMenu)
 							@if($taskMenu->pivot->status == 1)
-								@if(!$user->Tasks->isEmpty())
-									@foreach($user->Tasks as $taskUser)
-										@if($menu->id == $taskUser->pivot->menu_id && $taskMenu->id == $taskUser->pivot->task_id && $taskUser->pivot->status == 1)	
-						                    <div class="row text-center">
-												<input class="role-checkbox" type="checkbox" data-on-text="Activado" data-off-text="Desactivado" data-on-color="info" data-off-color="danger" data-label-text="{{$taskMenu->name}}" data-id="{{$taskMenu->id}}" checked>
-											</div>
-										@elseif($menu->id == $taskUser->pivot->menu_id && $taskMenu->id == $taskUser->pivot->task_id && $taskUser->pivot->status == 0)
-											<div class="row text-center">
-												<input class="role-checkbox" type="checkbox" data-on-text="Activado" data-off-text="Desactivado" data-on-color="info" data-off-color="danger" data-label-text="{{$taskMenu->name}}" data-id="{{$taskMenu->id}}">
-											</div>
-										@endif
-									@endforeach
-								@else
+								<?php $validator = 'false'; ?>
+								@foreach($user->Tasks as $taskUser)
+									@if($menu->id == $taskUser->pivot->menu_id && $taskMenu->id == $taskUser->pivot->task_id && $taskUser->pivot->status == 1)	
+					                    <div class="row text-center">
+											<input class="role-checkbox" type="checkbox" data-on-text="Activado" data-off-text="Desactivado" data-on-color="info" data-off-color="danger" data-label-text="{{$taskMenu->name}}" data-id="{{$taskMenu->id}}" checked>
+										</div>
+										<?php $validator = 'true' ?>
+									@elseif($menu->id == $taskUser->pivot->menu_id && $taskMenu->id == $taskUser->pivot->task_id && $taskUser->pivot->status == 0)
+										<div class="row text-center">
+											<input class="role-checkbox" type="checkbox" data-on-text="Activado" data-off-text="Desactivado" data-on-color="info" data-off-color="danger" data-label-text="{{$taskMenu->name}}" data-id="{{$taskMenu->id}}">
+										</div>
+										<?php $validator = 'true' ?>
+									@endif
+								@endforeach
+								@if($validator != 'true')
 									<div class="row text-center">
 										<input class="role-checkbox" type="checkbox" data-on-text="Activado" data-off-text="Desactivado" data-on-color="info" data-off-color="danger" data-label-text="{{$taskMenu->name}}" data-id="{{$taskMenu->id}}">
 									</div>
