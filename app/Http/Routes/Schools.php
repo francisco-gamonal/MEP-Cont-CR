ab5e7eaf2122d0e42966ee0971879743d559edc1
Francisco Gamonal <hfgamonalb@gmail.com>
2015-06-07 23:48:18 -0500

    fixed routes school

--- app/Http/Routes/Schools.php
+++ app/Http/Routes/Schools.php
@@ -7,13 +7,12 @@ use Illuminate\Support\Facades\Route;
 /*
  *  Rutas de Instituciones
  */
-Route::get('institucion', 'SchoolsController@index');
 Route::get('institucion/ver-institucion', ['as' => 'ver-institucion', 'uses' => 'SchoolsController@index']);
-Route::get('institucion/registrar-institucion', ['as' => 'registrar-institucion', 'uses' => 'SchoolsController@create']);
+Route::get('institucion/crear-institucion', ['as' => 'crear-institucion', 'uses' => 'SchoolsController@create']);
 Route::post('institucion/save-institucion', 'SchoolsController@store');
-Route::get('institucion/editar-institucion/{id}', ['as' => 'edit-school', 'uses' => 'SchoolsController@edit']);
-Route::delete('institucion/delete-institucion/{id}', ['as' => 'delete-school', 'uses' => 'SchoolsController@destroy']);
-Route::patch('institucion/active-institucion/{id}', ['as' => 'active-school', 'uses' => 'SchoolsController@active']);
+Route::get('institucion/editar-institucion/{id}', ['as' => 'editar-institucion', 'uses' => 'SchoolsController@edit']);
+Route::delete('institucion/delete-institucion/{id}', ['as' => 'delete-institucion', 'uses' => 'SchoolsController@destroy']);
+Route::patch('institucion/active-institucion/{id}', ['as' => 'active-institucion', 'uses' => 'SchoolsController@active']);
 Route::put('institucion/update-institucion/{id}', 'SchoolsController@update');
 /*
  * Fin Rutas de Instituciones
