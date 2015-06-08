f30b6ba687bd7371df42e61411fa7a7c433d559b
Francisco Gamonal <hfgamonalb@gmail.com>
2015-06-08 01:00:04 -0500

    fixed routes typeBudgets

--- app/Http/Routes/TypeBudget.php
+++ app/Http/Routes/TypeBudget.php
@@ -7,11 +7,10 @@ use Illuminate\Support\Facades\Route;
 /*
  * Rutas de Tipos de Usuarios
  */
-Route::get('tipos-de-presupuestos', 'TypeBudgetsController@index');
 Route::get('tipos-de-presupuestos/ver-tipos-de-presupuestos', ['as' => 'ver-tipos-de-presupuestos', 'uses' => 'TypeBudgetsController@index']);
-Route::get('tipos-de-presupuestos/registrar-tipo-de-presupuesto', ['as' => 'registrar-tipo-de-presupuesto', 'uses' => 'TypeBudgetsController@create']);
+Route::get('tipos-de-presupuestos/crear-tipos-de-presupuestos', ['as' => 'crear-tipos-de-presupuestos', 'uses' => 'TypeBudgetsController@create']);
 Route::post('tipos-de-presupuestos/save-tipos-de-presupuestos', 'TypeBudgetsController@store');
-Route::get('tipos-de-presupuestos/editar-tipo-de-presupuesto/{token}', ['as' => 'edit-tipo-de-presupuesto', 'uses' => 'TypeBudgetsController@edit']);
+Route::get('tipos-de-presupuestos/editar-tipos-de-presupuestos/{token}', ['as' => 'editar-tipos-de-presupuestos', 'uses' => 'TypeBudgetsController@edit']);
 Route::delete('tipos-de-presupuestos/delete-tipos-de-presupuestos/{token}', ['as' => 'delete-tipo-de-presupuesto', 'uses' => 'TypeBudgetsController@destroy']);
 Route::patch('tipos-de-presupuestos/active-tipos-de-presupuestos/{token}', ['as' => 'active-tipo-de-presupuesto', 'uses' => 'TypeBudgetsController@active']);
 Route::put('tipos-de-presupuestos/update-tipos-de-presupuestos', 'TypeBudgetsController@update');
--- resources/views/typeBudgets/index.blade.php
+++ resources/views/typeBudgets/index.blade.php
@@ -23,7 +23,7 @@
 							<h5><strong>Lista de Tipos de Presupuestos</strong></h5>
 						</div>
 						<div class="col-sm-6">
-							<a href="{{route('registrar-tipo-de-presupuesto')}}" class="btn btn-info pull-right">
+							<a href="{{route('crear-tipos-de-presupuestos')}}" class="btn btn-info pull-right">
 								<span class="glyphicon glyphicon-plus"></span>
 								<span>Nuevo</span>
 							</a>
@@ -63,7 +63,7 @@
 													<i class="fa fa-trash-o"></i>
 												</a>
 	                                		@endif
-											<a href="{{route('edit-tipo-de-presupuesto', $typeBudget->token)}}"><i class="fa fa-pencil-square-o"></i></a>
+											<a href="{{route('editar-tipos-de-presupuestos', $typeBudget->token)}}"><i class="fa fa-pencil-square-o"></i></a>
 		                                </td>
 		                            </tr>
 	                            @endforeach
