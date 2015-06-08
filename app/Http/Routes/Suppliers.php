7dbceb8afff2a4a371367c5bc9108f761d1f54a2
Francisco Gamonal <hfgamonalb@gmail.com>
2015-06-08 00:18:35 -0500

    fixed routes partial Suppliers

--- app/Http/Routes/Suppliers.php
+++ app/Http/Routes/Suppliers.php
@@ -7,11 +7,10 @@ use Illuminate\Support\Facades\Route;
 /*
  * Rutas de Proveedores
  */
-Route::get('proveedores', 'SupplierController@index');
 Route::get('proveedores/ver-proveedores', ['as' => 'ver-proveedores', 'uses' => 'SupplierController@index']);
-Route::get('proveedores/registrar-proveedor', ['as' => 'registrar-proveedor', 'uses' => 'SupplierController@create']);
+Route::get('proveedores/crear-proveedores', ['as' => 'crear-proveedores', 'uses' => 'SupplierController@create']);
 Route::post('proveedores/save-proveedores', 'SupplierController@store');
-Route::get('proveedores/editar-proveedor/{token}', ['as' => 'edit-proveedor', 'uses' => 'SupplierController@edit']);
+Route::get('proveedores/editar-proveedores/{token}', ['as' => 'editar-proveedores', 'uses' => 'SupplierController@edit']);
 Route::delete('proveedores/delete-proveedores/{token}', ['as' => 'delete-proveedor', 'uses' => 'SupplierController@destroy']);
 Route::patch('proveedores/active-proveedores/{token}', ['as' => 'active-proveedor', 'uses' => 'SupplierController@active']);
 Route::put('proveedores/update-proveedores/{token}', 'SupplierController@update');
