b4717f52634a9bf2e13c378fe16fa487ec3bf5ec
Francisco Gamonal <hfgamonalb@gmail.com>
2015-06-09 23:24:14 -0500

    ffinancing report budget

--- resources/views/reports/budget/title.blade.php
+++ resources/views/reports/budget/title.blade.php
@@ -1,7 +1,7 @@
 <div class="row">
 	<div style="font-size:12px; font-weight:bold; text-align:center;">
 		<p style="margin:0;">RELACIÓN DE INGRESOS Y GASTOS</p>
-		<p style="margin:0;">{{$budget->schools->ffinancing}}</p>
+		<p style="margin:0;">{{$budget->ffinancing}}</p>
 		<p style="margin:0;">(Del 01 de enero al 31 de diciembre del {{$budget->year}})</p>
 		<p style="margin:0 0 1em 0;">(en miles de colones)</p>
 	</div>
