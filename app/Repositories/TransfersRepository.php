ca6e65c8013dcb5ac6e117dc19d5f3d8195e5519
Anwar Sarmiento <anwarsarmiento@gmail.com>
2015-07-28 23:16:48 -0600

    filtro index transfer

--- app/Repositories/TransfersRepository.php
+++ app/Repositories/TransfersRepository.php
@@ -2,17 +2,34 @@
 namespace Mep\Repositories;
 
 use Mep\Entities\Transfer;
-
+use Mep\Repositories\BalanceBudgetRepository;
 /**
 * 
 */
 class TransfersRepository extends BaseRepository
 {
 		
+    private $balanceBudgetRepository;
+    
 
+    /**
+     * Create a new controller instance.
+     */
+    public function __construct(
+        BalanceBudgetRepository $balanceBudgetRepository
+        )
+    {
+       
+        $this->balanceBudgetRepository = $balanceBudgetRepository;
+       
+    }
 	
 	public function getModel()
 	{
 		return new Transfer();
+	}
+
+	public function listTransferIndex($campo, $budgetsId){
+		return $this->newQuery()->where($campo, $budgetsId)->whereIn('balance_budget_id',$this->balanceBudgetRepository->balanceBudgetListsSchool('id'))->get();
 	}
 }
